72904d4b0f48539e7cb74b18283b092cdeda0607
grsmith24 <gsmith24@cgps.org>
2024-02-29 11:42:28 -0500

    Class 2

--- Main.java
+++ Main.java
@@ -1,4 +1,25 @@
+import java.io.*;
+import java.util.*
+import java.util.ArrayList;
+
+
+
 public class Main {
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<String>();
+        File f =new File("names.txt");
+        try{
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String n = s.nextLine();
+                names.add(n);
+            }
+        }
+        catch (Exception e){
+
+        }
+    }
+
 
 
 }
