539020cd0b80a99e7ddcb1dedcbec432256e2bf1
grsmith24 <gsmith24@cgps.org>
2024-03-07 09:05:06 -0500

    Class 2

--- Trie.java
+++ Trie.java
@@ -14,19 +14,41 @@ public class Trie {
     // If not present, inserts key into trie
     // If the key is prefix of trie node,
     // just marks leaf node
-    public static void insert(String key) {
-     for(int i=0; i<key.length(); i++){
-         char currentLetter= key.charAt(i);
-         TrieNode[] kids = root.children;
-         for(TrieNode k: kids){
-             if(currentLetter == k.theLetter){
-                 //enter trie node
+    public static void insert(String key, TrieNode n) {
+        //go recursive
+        //go character by character
+        //base case: key.length()==0
+        if(key.length() <2>){
+            return;
+        }
+        else{
+            TrieNode[] kids = root.children;
 
-             }
-             else{
-                 //add letter
-             }
-         }
-     }
+            char currentLetter = key.charAt(0);
+            for(TrieNode k: kids){
+               if(currentLetter == k.theLetter){
+                   //enter trie node
+
+               }
+               else{
+                   //add letter
+               }
+           }
+            insert(key.substring(1), new TrieNode());
+
+        }
+//         for(int i=0; i<key.length(); i++){
+//             char currentLetter= key.charAt(i);
+//
+//             for(TrieNode k: kids){
+//                 if(currentLetter == k.theLetter){
+//                     //enter trie node
+//
+//                 }
+//                 else{
+//                     //add letter
+//                 }
+//             }
+//         }
     }
 }
